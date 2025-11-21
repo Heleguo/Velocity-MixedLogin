9f2666f23728fe30695e1845846f758050e432ae
mclk623 <mclk623@163.com>
2025-11-21 22:37:48 +0800

    fix build

--- api/src/main/java/fun/iiii/openvelocity/api/event/connection/OpenPreLoginEvent.java
+++ api/src/main/java/fun/iiii/openvelocity/api/event/connection/OpenPreLoginEvent.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2025 Velocity Contributors
+ *
+ * The Velocity API is licensed under the terms of the MIT License. For more details,
+ * reference the LICENSE file in the api top-level directory.
+ */
+
 package fun.iiii.openvelocity.api.event.connection;
 
 import com.google.common.base.Preconditions;
